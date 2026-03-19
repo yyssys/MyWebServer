0e76b44b28bd1223682b09d66ffa9efefe4b0f7e
yangshuang <atyangshuang@foxmail.com>
2026-03-19 11:55:05 +0800

    初始化服务器启动

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include "webServer.h"
 #include "config/config.h"
+#include "sql_conn_pool/sql_conn_pool.h"
 int main(int argc, char *argv[])
 {
+    // 数据库配置
+    const std::string url = "127.0.0.1:3306";
+    const std::string root = "root";
+    const std::string passwd = "110120";
+    const std::string dbname = "ys";
+
+
     // 命令行解析
     Config config;
     config.parseCommandLine(argc, argv);
     // 初始化日志系统
     Log::getInstance()->init(config);
-
+    // 初始化数据库连接池
+    MysqlConnPool::getInstance()->init(url, root, passwd, dbname, config.sqlConnCount);
     // 启动服务器
     webServer server(config);
     server.run();
